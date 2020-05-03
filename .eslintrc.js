dd9541d42e3bc25c31c969d452f8df2547b613a1
gabrielkf <gabrielkf@gmail.com>
2020-05-03 19:41:15 -0300

    defined file structure; configured eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react'],
+  rules: {
+    'prettier/prettier': 2,
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.jsx', '.js'] },
+    ],
+    'import/prefer-default-export': 0,
+    'react/state-in-constructor': 0,
+    'react/static-property-placement': 0,
+    'no-unused-vars': 1,
+    'no-param-reassign': 0,
+    'no-console': 0,
+    'jsx-a11y/control-has-associated-label': 0,
+  },
+};
---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+// import { Container } from './styles';
+
+class Dashboard extends Component {
+  return <div />;
+}
+
+export default Room;
